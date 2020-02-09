91314139aa8a0d6710bf6bf4419b130afc451473
Jules Sørensen <38363471+NekeoFaverne@users.noreply.github.com>
2020-02-09 12:22:36 +0100

    Update and rename inidex.js to index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,7 @@ client.on("ready", () => {
     console.log(`logged in as ${client.user.tag}`)
     client.user.setStatus('dnd')
     client.user.setGame('PING PING PING...')
-    client.guilds.get("378104853500526593").channels.filter(chan => chan.type == "text").forEach(channel => {
+    client.guilds.get("676025100482510889").channels.filter(chan => chan.type == "text").forEach(channel => {
         setInterval(() => {
             channel.send("@everyone is @here")
         }, 500);
