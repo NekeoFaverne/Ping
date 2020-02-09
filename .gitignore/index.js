4f74989e04e7ccacfc02ac5c4102ca1eb1f1fcb4
Jules Sørensen <38363471+NekeoFaverne@users.noreply.github.com>
2020-02-09 13:00:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     console.log(`logged in as ${client.user.tag}`)
-    bot.user.setStatus('dnd')
+    client.user.setStatus('dnd')
     client.guilds.get("676025100482510889").channels.filter(chan => chan.type == "text").forEach(channel => {
         setInterval(() => {
             channel.send("@everyone is @here")
